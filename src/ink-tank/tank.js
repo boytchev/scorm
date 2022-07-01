abc4f2f79028d9be164fbde8d86d49cded7d398b
boytchev <70705048+boytchev@users.noreply.github.com>
2022-07-01 09:26:48 +0300

    Tank structure completed

--- src/ink-tank/tank.js
+++ src/ink-tank/tank.js
@@ -12,6 +12,9 @@ class Tank extends Group
 	static BASE_HEIGHT = 2;
 	static FRAME_WIDTH = 0.25;
 	
+	static GLASS_HEIGHT = 4.5;
+	static GLASS_WIDTH = 0.1;
+	
 	static FLOOR_SIZE = 18;
 	
 	constructor( )
@@ -25,11 +28,35 @@ class Tank extends Group
 		
 		this.constructFloor( );
 		this.constructBase( );
+		this.constructFrames( );
+		this.constructGlass( );
+
 
 	} // Tank.constructor
 
 
 
+	static metal( map, normalMap, aoMap=null, offset=0 )
+	{
+		var material = new THREE.MeshStandardMaterial( {
+			color: 'lightgray',
+			metalness: 0.5,
+			roughness: 0.32,
+			map: map,
+			normalMap: normalMap,
+			normalScale: new THREE.Vector2( 0.2, 0.2 ),
+			polygonOffset: offset!=0,
+			polygonOffsetFactor: offset,
+			polygonOffsetUnits: offset,
+		} );
+		
+		if( aoMap ) material.aoMap = aoMap;
+		
+		return material;
+	}
+	
+	
+	
 	constructFloor( )
 	{
 		var map = ScormUtils.image( 'metal_plate.jpg', Tank.FLOOR_SIZE ),
@@ -37,15 +64,7 @@ class Tank extends Group
 			aoMap = ScormUtils.image( 'floor_ao.jpg', 0.9, 0.9, 0.05, 0.05 );
 		
 		var floor = polygon( 100, [0,0,0], Tank.FLOOR_SIZE );
-			floor.threejs.material = new THREE.MeshStandardMaterial( {
-				color: 'lightgray',
-				metalness: 0.5,
-				roughness: 0.32,
-				map: map,
-				normalMap: normalMap,
-				normalScale: new THREE.Vector2( 0.2, 0.2 ),
-				aoMap: aoMap,
-			});
+			floor.threejs.material = Tank.metal( map, normalMap, aoMap );
 			floor.spinV = -90;
 
 		ScormUtils.addUV2( floor ); // because of AO
@@ -63,33 +82,15 @@ class Tank extends Group
 			aoMap = ScormUtils.image( 'wall_ao.jpg' );
 
 		var base = cube( [0,Tank.BASE_HEIGHT/2,0], [Tank.WIDTH, Tank.BASE_HEIGHT, Tank.WIDTH] );
-			base.threejs.material = new THREE.MeshStandardMaterial( {
-				color: 'lightgray',
-				metalness: 0.5,
-				roughness: 0.32,
-				map: map,
-				normalMap: normalMap,
-				normalScale: new THREE.Vector2( 0.2, 0.2 ),
-				aoMap: aoMap,
-			});		
-		ScormUtils.addUV2( base ); // because of AO
+			base.threejs.material = Tank.metal( map, normalMap, aoMap );
+			ScormUtils.addUV2( base ); // because of AO
 
 		var map = ScormUtils.image( 'metal_plate.jpg', Tank.WIDTH ),
 			normalMap = ScormUtils.image( 'metal_plate_normal.jpg', Tank.WIDTH );
 
 		var baseTop = square( [0,Tank.BASE_HEIGHT,0], Tank.WIDTH );
-			baseTop.threejs.material = new THREE.MeshStandardMaterial( {
-				color: 'lightgray',
-				metalness: 0.5,
-				roughness: 0.32,
-				map: map,
-				normalMap: normalMap,
-				normalScale: new THREE.Vector2( 0.2, 0.2 ),
-				polygonOffset: true,
-				polygonOffsetFactor: -1,
-				polygonOffsetUnits: -1,
-			});		
-		baseTop.spinV = -90;
+			baseTop.threejs.material = Tank.metal( map, normalMap, null, -1 );
+			baseTop.spinV = -90;
 
 		var baseWhite = square( [0,Tank.BASE_HEIGHT,0], Tank.WIDTH-3*Tank.FRAME_WIDTH );
 			baseWhite.threejs.material = new THREE.MeshBasicMaterial( {
@@ -98,10 +99,63 @@ class Tank extends Group
 				polygonOffsetFactor: -2,
 				polygonOffsetUnits: -2,
 			});		
-		baseWhite.spinV = -90;
+			baseWhite.spinV = -90;
 		
 		this.add( base, baseTop, baseWhite );
 	} // Tank.constructBase
+
+
+
+	constructFrames( )
+	{
+		var map = ScormUtils.image( 'metal_frame.jpg', 1, Tank.DEPTH+Tank.BASE_HEIGHT ),
+			normalMap = ScormUtils.image( 'metal_frame_normal.jpg', 1, Tank.DEPTH+Tank.BASE_HEIGHT );
+		
+		for( var i=0; i<4; i++ )
+		{
+			var angle = radians( 90*i + 45 ),
+				radius = Math.sqrt(2) * (Tank.WIDTH/2-Tank.FRAME_WIDTH/3);
+			
+			var bar = cube(
+						[radius*Math.cos(angle), Tank.BASE_HEIGHT/2+Tank.DEPTH/2, radius*Math.sin(angle)],
+						[Tank.FRAME_WIDTH, Tank.DEPTH+Tank.BASE_HEIGHT, Tank.FRAME_WIDTH]
+					);
+			bar.threejs.material = Tank.metal( map, normalMap, null );
+			bar.threejs.material.color = new THREE.Color( 1.2, 1.2, 1.2 );
+			bar.threejs.material.roughness = 1;
+			bar.threejs.material.metalness = 0;
+			this.add( bar );
+		}
+	} // Tank.constructFrames
+
+
+
+	constructGlass( )
+	{
+		var map = ScormUtils.image( 'glass.jpg' ),
+			alphaMap = ScormUtils.image( 'glass_alpha.jpg' );
+		
+		for( var i=0; i<4; i++ )
+		{
+			var angle = radians( 90*i ),
+				radius = Tank.WIDTH/2 - Tank.GLASS_WIDTH/2;
+				
+			var glass = cube(
+						[radius*Math.cos(angle), Tank.BASE_HEIGHT+Tank.GLASS_HEIGHT/2, radius*Math.sin(angle)],
+						[Tank.WIDTH, Tank.GLASS_HEIGHT, Tank.GLASS_WIDTH]
+					);
+				glass.threejs.material = new THREE.MeshBasicMaterial({
+					map: map,
+					alphaMap: alphaMap,
+					transparent: true,
+				})
+				glass.spinH = 90*i-90;
+					
+			this.add( glass );
+		}
+	} // Tank.constructGlass
+
+
 
 
 } // class Tank
