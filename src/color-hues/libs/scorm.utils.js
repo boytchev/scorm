6ea05e621bd06c6f9e0eb4ace138f6e787d9c2b0
boytchev <70705048+boytchev@users.noreply.github.com>
2026-06-30 12:00:47 +0300

    hands shift again

--- src/color-hues/libs/scorm.utils.js
+++ src/color-hues/libs/scorm.utils.js
@@ -353,7 +353,7 @@ class ScormPlayground
 
 			for( var i=0; i<pos.count; i++ )
 			{
-				var z = pos.getZ( i ) - 10;
+				var z = pos.getZ( i ) - 3;
 				if( pos.getZ(i) < -4.1 ) {
 					controller.indices.push( i );
 					controller.positions.push( z );
